70c25faa117f413966d7dfec37ca8bb659caa3c6
cameron <hammerbro_97@hotmail.com>
2023-04-04 19:53:56 -0600

    added API

--- js/app.js
+++ js/app.js
@@ -1,20 +1,33 @@
 
-function successFunction(success_looper){
+function successFunction(response){
+let post_container = document.getElementById(`post_container`);
+   
+for(let counter = 0; counter<response[`data`].length; counter = counter +1){
+post_container.insertAdjacentHTML(`beforeend`, `
 
+<span>
+<h3>${response[`data`][counter][`title`]}</h3>
+<p>${response[`data`][counter][`body`]}</p>
+</span>
+`);
 
+}
 
 
 };
 
 
 function failureFunction(fail_looper){
+    let post_container = document.getElementById(`post_container`);
+post_container.insertAdjacentHTML(`beforeend`, `<h1>something has gona wrong.</h1>`);
 
-
-    
-}
+};
 
 
 axios.request({
 
     url: `https://jsonplaceholder.typicode.com/posts`
 }).then(successFunction).catch(failureFunction);
+
+
+
